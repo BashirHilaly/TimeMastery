61ac5c60df75447ce493e97a1c37b35089180cd2
BashirHilaly <bashirkenza@gmail.com>
2024-03-17 07:21:32 -0400

    Added NativeWind

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
         <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View>
-        <Text>WITH TAILWINDCSS!</Text>
-      </View>
+        <Text className="text-xl font-bold text-orange-600">WITH TAILWINDCSS!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
