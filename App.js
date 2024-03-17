dbeb5feef042a5532b766546a7890e5e98cfa481
BashirHilaly <bashirkenza@gmail.com>
2024-03-17 10:24:42 -0400

    added summary component

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Summary from './components/Summary';
+import { FontAwesome } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text className="text-xl font-bold text-orange-600">WITH TAILWINDCSS!</Text>
+    <View className="flex-1 items-center bg-[#141319] min-h-screen">
+      <Summary />
+      <Text className="text-[#5C5A5A] [text-shadow:_0_2px_4px_rgb(0_0_0_/_30%)] mt-2">Drag task to start timer</Text>
+      <View className='-mt-2'>
+        <FontAwesome name="long-arrow-right" size={22} color="#5C5A5A" />
+      </View>
+      <View className="container h-64 flex flex-row w-10/12">
+        <View className="basis-1/2 items-start">
+          <LinearGradient
+          // Background Linear Gradient
+          colors={['rgba(0,0,0,0.8)', 'transparent']}
+          style={{ flex: 1 }}
+          >
+            <View className="bg-slate-500 h-64 w-11/12 p-5 rounded-3xl">
+              <Text className="text-[#BEBEBE] font-bold text-xl text-center">ADD TASK</Text>
+            </View>
+          </LinearGradient>
+        </View>
+        <View className="basis-1/2 items-end">
+          <LinearGradient
+            // Background Linear Gradient
+            colors={['rgba(0,0,0,0.8)', 'transparent']}
+            style={{ flex: 1 }}
+          >
+            <View className="bg-slate-500 h-64 w-11/12 p-5 rounded-3xl">
+              <Text className="text-[#BEBEBE] font-bold text-xl text-center">CLOCK</Text>
+            </View>
+          </LinearGradient>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
+
+const styles = StyleSheet.create({
+
+});
---
+++ components/Summary.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Summary = () => {
+  return (
+    <View className="container mt-16 w-10/12 rounded-3xl bg-[#27252F] justify-center p-6">
+        <Text className="text-[#BEBEBE] text-center font-bold text-2xl">SUMMARY</Text>
+        <View className="flex flex-row mt-3">
+            <View className="basis-1/2">
+                <Text className="text-[#BEBEBE]">Circle</Text>
+            </View>
+            <View className="basis-1/2">
+                <View className="flex flex-row mb-4">
+                    <View className="basis-1/2 justify-center items-center">
+                        <FontAwesome name="circle" size={18} color="#CFAADF" />
+                    </View>
+                    <View className="basis-1/2">
+                        <Text className="text-[#BEBEBE] font-semibold text-xl">Task 1</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row mb-4">
+                    <View className="basis-1/2 justify-center items-center">
+                        <FontAwesome name="circle" size={18} color="#FEDA98" />
+                    </View>
+                    <View className="basis-1/2">
+                        <Text className="text-[#BEBEBE] font-semibold text-xl">Task 2</Text>
+                    </View>
+                </View>
+                <View className="flex flex-row mb-4">
+                    <View className="basis-1/2 justify-center items-center">
+                        <FontAwesome name="circle" size={18} color="#6EEADF" />
+                    </View>
+                    <View className="basis-1/2">
+                        <Text className="text-[#BEBEBE] font-semibold text-xl">Task 3</Text>
+                    </View>
+                </View>                
+            </View>
+            
+            
+        </View>
+    </View>
+  );
+};
+
+export default Summary;
